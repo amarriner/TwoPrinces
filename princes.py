8486e3f38970a2b42e8cc483fd9ef430ec8790f1
Aaron Marriner <amarriner@amarriner.com>
2014-08-06 13:26:42 +0000

    Skeleton of script

---
+++ princes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Twitter bot that tweets random (fake, obvs) drafts of the first line of Two Princes by the Spindoctors"""
+
+import sys
+
+
+def main():
+   """Main entry point"""
+
+   return 1
+
+
+if __name__ == '__main__':
+   sys.exit(main())
+
